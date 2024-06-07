bbb40424c66a4a363e82404c849eb34001554d52
Ecommerce <103959491+Barnelaswathi77@users.noreply.github.com>
2024-06-07 10:45:52 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,7 @@
+'use client'
+import React, { useState } from 'react';
+import Count from "./count.js";
+import "./color.css";
+import Row from "./row.js";
+import {movies} from "./data.js";
+export default function App(){
